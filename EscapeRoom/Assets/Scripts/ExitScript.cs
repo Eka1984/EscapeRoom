38e91f10f095084d05b9413e7b982b0c7752f871
Katja <katja.korzneva@gmail.com>
2023-12-01 12:43:21 +0200

    Exit Button huoneisiin

---
+++ EscapeRoom/Assets/Scripts/ExitScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ExitScript : MonoBehaviour
+{
+    public string targetScene;
+
+    // Kun exitobjektia klikataan, päästään seruaavalle tasolle
+    private void OnMouseDown()
+    {
+        
+        SceneManager.LoadScene(targetScene);
+    }
+}
