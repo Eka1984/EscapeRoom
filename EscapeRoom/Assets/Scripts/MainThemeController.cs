7ca97df302e81b4e4ec33f4727a0a1fd831b6ad4
Katja <katja.korzneva@gmail.com>
2023-12-05 16:50:05 +0200

    Music is added to all the scenes

---
+++ EscapeRoom/Assets/Scripts/MainThemeController.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Video;
+
+public class MainThemeController : MonoBehaviour
+{
+    private AudioSource audioSource;
+    private VideoPlayer videoPlayer;
+
+    
+    private void Start()
+    {
+        
+        DontDestroyOnLoad(gameObject);
+
+        
+        audioSource = GetComponent<AudioSource>();
+
+        
+        audioSource.Play();
+
+        
+        int lastSceneIndex = SceneManager.sceneCountInBuildSettings - 1;
+        if (SceneManager.GetActiveScene().buildIndex == lastSceneIndex)
+        {            
+            StopMusic();
+        }        
+        
+    }
+
+    private void Update()
+    {
+        videoPlayer = FindObjectOfType<VideoPlayer>();
+
+        if (videoPlayer != null)
+        {
+            videoPlayer.started += VideoStarted;
+            videoPlayer.loopPointReached += VideoStopped;
+
+            if (videoPlayer.isPaused)
+            {
+                VideoPaused(videoPlayer);
+            }
+
+        }
+    }
+
+    private void StopMusic()
+    {       
+        audioSource.Stop();
+    }
+
+    private void VideoStarted(VideoPlayer vp)
+    {        
+        audioSource.Pause();        
+    }
+
+    private void VideoStopped(VideoPlayer vp)
+    {
+        ResumeMusic();
+    }
+
+    private void VideoPaused(VideoPlayer vp)
+    {                  
+       ResumeMusic();        
+    }
+
+    private void ResumeMusic()
+    {        
+        audioSource.UnPause();        
+    }  
+
+}
+
