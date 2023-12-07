5976d130e0c422954821e5b57c07b4b5e28685ab
Katja <katja.korzneva@gmail.com>
2023-12-07 16:16:58 +0200

    Scene 2 Done

---
+++ EscapeRoom/Assets/Scripts/TextChangeScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextChangeScript : MonoBehaviour
+{
+    public TextMeshProUGUI panelText;
+
+    void Start()
+    {
+        //Aloitetaan coroutine 
+        StartCoroutine(ChangeTextAfterDelay(5f));
+    }
+
+    IEnumerator ChangeTextAfterDelay(float delay)
+    {
+        // Odotetaan määriteltyä aikaa
+        yield return new WaitForSeconds(delay);
+
+        // Vaihdetaan teksti
+        if (panelText != null)
+        {
+            panelText.text = "Nyt on aika rentoutua \nja \nsuunnata joululomalle! \n\nHyvää Joulua!<3";
+        }
+    }
+}
