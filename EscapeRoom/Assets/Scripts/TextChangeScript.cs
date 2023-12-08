1f206f5a1b6ae652b041078ca37b09ec582accc5
matildalalli <matilda.lalli@edu.lapinamk.fi>
2023-12-08 14:24:36 +0200

    Korjauksia
    Korjasin VRCameran skriptin, jossa kamera jatkoi liikkumista vaikka hiirtä ei liikuttanut. Korjasin myös paneelien kokoa. Lisäsin Exit buttonin pelin loppuun, josta pääsee pois pelistä. Lisäsin myös valoja pariin sceneen

--- EscapeRoom/Assets/Scripts/TextChangeScript.cs
+++ EscapeRoom/Assets/Scripts/TextChangeScript.cs
@@ -1,15 +1,19 @@
-﻿using System.Collections;
+﻿using JetBrains.Annotations;
+using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class TextChangeScript : MonoBehaviour
 {
+    public Button ExitGame;
     public TextMeshProUGUI panelText;
 
     void Start()
     {
+        ExitGame.interactable = false;
         //Aloitetaan coroutine 
         StartCoroutine(ChangeTextAfterDelay(5f));
     }
@@ -23,6 +27,13 @@ public class TextChangeScript : MonoBehaviour
         if (panelText != null)
         {
             panelText.text = "Nyt on aika rentoutua \nja \nsuunnata joululomalle! \n\nHyvää Joulua!<3";
+            ExitGame.interactable = true;
         }
+
+
+    }
+    public void LopetusBtnClicked()
+    {
+        Application.Quit();
     }
 }
--- EscapeRoom/Assets/Scripts/VRCamera.cs
+++ EscapeRoom/Assets/Scripts/VRCamera.cs
@@ -1,30 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using System;
+
 public class VRCamera : MonoBehaviour
-{ // flag to keep track whether we are dragging or not
-    public  bool isDragging = false;
+{
+    // flag to keep track whether we are dragging or not
+    private bool isDragging = false;
 
     // starting point of a camera movement
-    float startMouseX;
-    float startMouseY;
+    private float startMouseX;
+    private float startMouseY;
 
     // Camera component
-    public  Camera cam;
-    // Start is called before the first frame update
+    private Camera cam;
+
+    // sensitivity for camera movement
+    public float sensitivity = 1.0f;
+
     void Start()
     {
         // Get our camera component
-        cam = this.gameObject.GetComponent<Camera>();
+        cam = GetComponent<Camera>();
     }
 
-	// Update is called once per frame
-	void Update () {
-		    
+    void Update()
+    {
         // if we press the left button and we haven't started dragging
-        if(Input.GetMouseButtonDown(0) && !isDragging )
-        {                
+        if (Input.GetMouseButtonDown(0) && !isDragging)
+        {
             // set the flag to true
             isDragging = true;
 
@@ -33,8 +36,8 @@ public class VRCamera : MonoBehaviour
             startMouseY = Input.mousePosition.y;
         }
         // if we are not pressing the left btn, and we were dragging
-        else if(Input.GetMouseButtonUp(0) && isDragging)
-        {                
+        else if (Input.GetMouseButtonUp(0) && isDragging)
+        {
             // set the flag to false
             isDragging = false;
         }
@@ -42,28 +45,28 @@ public class VRCamera : MonoBehaviour
 
     void LateUpdate()
     {
-        // Check if we are dragging
-         if(isDragging)
+        // Check if we are dragging and mouse is moving
+        if (isDragging && (Input.GetAxis("Mouse X") != 0 || Input.GetAxis("Mouse Y") != 0))
         {
-            //Calculate current mouse position
+            // Calculate current mouse position
             float endMouseX = Input.mousePosition.x;
             float endMouseY = Input.mousePosition.y;
 
-            //Difference (in screen coordinates)
-            float diffX = endMouseX - startMouseX;
-            float diffY = endMouseY - startMouseY;
+            // Difference (in screen coordinates)
+            float diffX = (endMouseX - startMouseX) * sensitivity;
+            float diffY = (endMouseY - startMouseY) * sensitivity;
 
-            //New center of the screen
+            // New center of the screen
             float newCenterX = Screen.width / 2 + diffX;
             float newCenterY = Screen.height / 2 + diffY;
 
-            //Get the world coordinate , this is where we want to look at
-            Vector3 LookHerePoint = cam.ScreenToWorldPoint(new Vector3(newCenterX, newCenterY, cam.nearClipPlane));
+            // Get the world coordinate, this is where we want to look at
+            Vector3 lookHerePoint = cam.ScreenToWorldPoint(new Vector3(newCenterX, newCenterY, cam.nearClipPlane));
 
-            //Make our camera look at the "LookHerePoint"
-            transform.LookAt(LookHerePoint);
+            // Make our camera look at the "lookHerePoint"
+            transform.LookAt(lookHerePoint);
 
-            //starting position for the next call
+            // Starting position for the next call
             startMouseX = endMouseX;
             startMouseY = endMouseY;
         }
