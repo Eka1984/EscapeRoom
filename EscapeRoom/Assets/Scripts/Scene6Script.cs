4bc1321f61c13495c072f505afa95d313be9a5ae
HitomiRuoho <hruoho@edu.lapinamk.fi>
2023-12-09 22:24:52 +0200

    Scene6 kommentti on lisätty

--- EscapeRoom/Assets/Scripts/Scene6Script.cs
+++ EscapeRoom/Assets/Scripts/Scene6Script.cs
@@ -17,105 +17,66 @@ public class Scene6Script : MonoBehaviour
     public GameObject quetion;
     public GameObject onnittelutTxt;
     public GameObject panel;   
-    //public Animator onnittelutPanelAnimator;
-    //bool hasPlayedAnimation;
-
 
 
     // Start is called before the first frame update
     void Start()
     {
+        // exit kuva ei ole n‰kyville
         exitPicture.enabled = false;
         exitPictureCollider.enabled = false;
-
-        //onnittelutPanelAnimator = GetComponent<Animator>();
-        //hasPlayedAnimation = false;
-
+        
+        // animaatio ei n‰kyville, joka ilmestyy kun vastaus on oikein
         onnittelutTxt.GetComponent<Animator>().Play("Wait");
-
-        //Animator onnittelutPanelAnimator = onnittelutTxt.GetComponent<Animator>();
-        //if (onnittelutPanelAnimator != null)
-        //{
-        //    Debug.Log("Start: Current Animation State: " + onnittelutPanelAnimator.GetCurrentAnimatorStateInfo(0).fullPathHash);
-        //}
-        //else
-        //{
-        //    Debug.LogError("Animator component not found on Onnittelut_Panel GameObject.");
-        //}
-        //projektiTxt.GetComponent<Animator>().Play("Project");s
-
-        //onnittelutPanelAnimator = GetComponent<Animator>();
-
-        //if (onnittelutPanelAnimator == null)
-        //{
-        //    Debug.LogError("Animator component not found on Onnittelut_Panel GameObject.");
-        //}
     }
 
     // Update is called once per frame
     void Update()
     {
+        // vastaus on v‰‰rin 
         if (answer != oikeinVastaus)
         {
+            // vastauksen vaihtoehdot ovat esille
             bt1.SetActive(true);
             bt2.SetActive(true);
             bt3.SetActive(true);
             onnittelutTxt.GetComponent<Animator>().Play("Wait");
         }
+        // vastaus on oikein
         else if(answer == oikeinVastaus)
         {
+            // vastauksen vaihtoehdot menev‰t piiloon
             bt1.SetActive(false);
             bt2.SetActive(false);
             bt3.SetActive(false);
             quetion.SetActive(false);
-            panel.SetActive(false);
-            //onnittelutTxt.SetActive(true); //onnittelutTxt.transform.SetSiblingIndex(0);  
-            
+            panel.SetActive(false);          
 
             // exitkuva tulee esille
             exitPicture.enabled = true;
             exitPictureCollider.enabled = true;
 
+            // animaatio ilmestyy
             onnittelutTxt.GetComponent<Animator>().Play("Onnittelut_Panel");
-            //if(hasPlayedAnimation == false)
-            //{
-            //    onnittelutPanelAnimator.SetBool("hasPlayedAnimation", false);
-            //}
-            //if (hasPlayedAnimation == true)
-            //{
-            //    onnittelutPanelAnimator.SetBool("hasPlayedAnimation", true);
-            //}
-
-
-            //onnittelutTxt.GetComponent<Animator>().Play("Onnittelut_Panel");
-
-            //Animator onnittelutPanelAnimator = onnittelutTxt.GetComponent<Animator>();
-            //if (onnittelutPanelAnimator != null)
-            //{
-            //    Debug.Log("Update: Current Animation State: " + onnittelutPanelAnimator.GetCurrentAnimatorStateInfo(0).fullPathHash);
-            //    onnittelutPanelAnimator.Play("Onnittelut_Panel");
-            //    //hasPlayedAnimation = true; // Set the flag to true to prevent re-triggering the animation
-            //    Debug.Log("Animation Played!");
-            //}
-            //else
-            //{
-            //    Debug.LogError("Animator component not found on Onnittelut_Panel GameObject.");
-            //}
+            
         }
     }
 
+    // funktio vastaus Btn1
     public void btn1Clicked()
     {
         textBox.text = "Vastaus on v‰‰rin!";
         answer = 0;
     }
 
+    // funktio vastaus Btn2
     public void btn2Clicked()
     {
         textBox.text = "Vastaus on v‰‰rin!";
         answer = 1;
     }
 
+    // funktio vastaus Btn3
     public void btn3Clicked()
     {
         textBox.text = "Vastaus on oiekin!";        
